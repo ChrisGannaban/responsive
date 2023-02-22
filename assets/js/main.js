959083986e20b292698f146137a9d4bda46b9281
Chris Gannaban <christophergannaban@voncore.net>
2023-02-22 12:52:52 +0800

    css style format

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal({
+    distance: "100px",
+    duration: 2000,
+    delay: 400,
+  });
+  
+sr.reveal(".about__img", { origin: "left" });
+sr.reveal(".about__content", { origin: "right" });
